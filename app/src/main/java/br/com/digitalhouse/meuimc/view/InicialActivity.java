6b572e1b39df076f58ac0414fbc47e37112ea0a4
Filipe <fil.desouza@gmail.com>
2019-09-21 09:34:09 -0300

    projeto revisao meuIMC

---
+++ app/src/main/java/br/com/digitalhouse/meuimc/view/InicialActivity.java
@@ -0,0 +1,16 @@
+package br.com.digitalhouse.meuimc.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.digitalhouse.meuimc.R;
+
+public class InicialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicial);
+    }
+}
